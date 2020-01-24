25afff38b84dc45922686a35c2222706126a7634
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-23 20:59:11 -0600

    linked html files together and created server.js to load index.html

--- server.js
+++ server.js
@@ -1,21 +1,39 @@
 //======================================================================
 // Welcome to my Note-Taker, based on Express.js.
-//======================================================================
-// My dependencies; I'm unhealthily co-dependent.
+
+// Below are my dependencies; I'm unhealthily co-dependent.
 //======================================================================
 
 const mySQL = require("mysql");
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 
 //======================================================================
 // This sets up the Express App
 //======================================================================
 
 var app = express();
-var PORT = 5000;
+var PORT = process.env.PORT || 5001;
 
 //==============================================================================
-//This sets up data parsing-- Express will interpret it/format data as JSON.
+// Gotta link to my assets!
+app.use(express.static('public'));
+
+//==============================================================================
+// This sets up data parsing-- Express will interpret it/format data as JSON.
+// This is required for API calls!
 //==============================================================================
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//==============================================================================
+// On page load, it should start with index.html. First get it and then listen.
+//==============================================================================
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+});
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
